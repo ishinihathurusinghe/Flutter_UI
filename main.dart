2d9ff75fd653010f0d5edcf32da4286fa92c15aa
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2023-05-03 09:31:42 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 //import 'package:widget_ecplain_app/homepage.dart';
 import 'package:widget_ecplain_app/widgets/listviewbuilder.dart';
 import 'package:widget_ecplain_app/screens/solo_adventure.dart';
